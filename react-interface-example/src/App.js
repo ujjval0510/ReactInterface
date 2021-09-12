ebcaf28debfbbcca53d9dd98ab44b535c55cafb1
ujjvalVebuin <ujjvalshrimali@vebuin.com>
2021-09-13 01:52:24 +0900

    Restructure appointment component.
    Restructure appointment component.

--- react-interface-example/src/App.js
+++ react-interface-example/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { BiCalendar, BiTrash } from 'react-icons/bi';
+import { BiCalendar } from 'react-icons/bi';
 import Search from './components/Search';
 import AddAppointment from './components/AddAppointment';
 import appointmentList from './data.json';
+import AppointmentInfo from './components/AppointmentInfo';
 
 function App() {
   return (
@@ -14,22 +15,9 @@ function App() {
       <Search />
 
       <ul className="divide-y divide-gray-200">
-        {
-          appointmentList.map((appointment) => (
-            <li className="px-3 py-3 flex items-start">
-              <button type="button"
-                className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-500 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                <BiTrash /></button>
-              <div className="flex-grow">
-                <div className="flex items-center">
-                  <span className="flex-none font-medium text-2xl text-blue-500">{appointment.petName}</span>
-                  <span className="flex-grow text-right">{appointment.aptDate}</span>
-                </div>
-                <div><b className="font-bold text-blue-500">Owner:</b> {appointment.ownerName}</div>
-                <div className="leading-tight">{appointment.aptNotes}</div>
-              </div>
-            </li>
-          ))}
+        {appointmentList.map((appointment) => (
+          <AppointmentInfo key={appointment.id} appointment={appointment} />
+        ))}
       </ul>
     </div>
   );
